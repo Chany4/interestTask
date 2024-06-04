af8a0def6ea7a880878e9c763938cddbf02aa1f5
Chany4 <chandreleighdavids04052005@gmail.com>
2024-06-04 15:53:40 +0200

    fixed

--- script/script.js
+++ script/script.js
@@ -1,19 +1,12 @@
-
-// interset amount 
-let p =document.querySelector('#p');
-let r = document.querySelector('#r');
-let t = document.querySelector('#t');
-let btn2 = document.querySelector('.intBtn')
-let span2 = document.querySelector('.intConv')
-
-
-let interest = (p * r * t) / 100;
-
-console.log("Simple Interest: " + interest);
-let total = p + interest;
-console.log("Total Amount: " + total);
-
-
-btn2.addEventListener('click', ()=>{
-    span2.innerHTML = ['Simple Interest is ' + interest + 'and toatl amount is ' + total]
-})
+document.querySelector('.intBtn').addEventListener('click', function() {
+    // Get the input values
+    const p = parseFloat(document.getElementById('p').value);
+    const r = parseFloat(document.getElementById('r').value);
+    const t = parseFloat(document.getElementById('t').value);
+    
+    // Calculate the interest
+    const interest = (p * r * t) / 100;
+    
+    // Display the result
+    document.querySelector('.intConv').innerText = interest + " %";
+});
